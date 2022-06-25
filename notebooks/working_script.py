5218e24b954f2c7d267469dcbbd65629d68c17ff
Krzysztof Chojnacki <krzysztof.michal.chojnacki@gmail.com>
2022-06-25 19:49:23 +0200

    Update working_script.py

--- notebooks/working_script.py
+++ notebooks/working_script.py
@@ -13,7 +13,7 @@ from imblearn.over_sampling import SMOTE
 from sklearn.metrics import confusion_matrix, balanced_accuracy_score
 from sklearn.model_selection import train_test_split
 from os import chdir
-from sklearn.decomposition import PCA, KernelPCA
+from sklearn.decomposition import KernelPCA
 import joblib
 
 path = r"C:\Users\krzys\Desktop\data science\IV semestr\machine_learning_2\projekt"
@@ -22,7 +22,7 @@ chdir(path)
 
 
 def loading_files(path):
-    '''
+    """
     Function allows you to load labels and target.
 
     Parameters
@@ -32,18 +32,18 @@ def loading_files(path):
     Returns
     -------
     X : DataFrame
-    
+
     y : DataFrame
 
-    '''
+    """
     X = pd.read_csv("train_data.csv", header=None)
     y = pd.read_csv("train_labels.csv", header=None)
     return X, y
 
 
 def binarization(y):
-    '''
-    
+    """
+    Fucntion used to represent categorical data as numbers.
 
     Parameters
     ----------
@@ -55,16 +55,16 @@ def binarization(y):
     y : DataFrame
         Target variable after binarization.
 
-    '''
+    """
     y_binar = binarize(y)
     y = pd.DataFrame(np.ravel(y_binar))
     return y
 
 
-def principal_component_analysis(X, n_components=1012):
+def kernel_principal_component_analysis(X, n_components=1012):
     global kpca
-    '''
-    
+    """
+    Non-linear dimensionality reduction through the use of kernels 
 
     Parameters
     ----------
@@ -78,7 +78,7 @@ def principal_component_analysis(X, n_components=1012):
     X_pca : DataFrame
         After dimensions reduction. By default 95% of variance.
 
-    '''
+    """
     kpca = KernelPCA(n_components=n_components)
     X_pca = kpca.fit_transform(X)
     X_pca = pd.DataFrame(X_pca)
@@ -86,8 +86,8 @@ def principal_component_analysis(X, n_components=1012):
 
 
 def splitting_data(X, y):
-    '''
-    
+    """
+    Splitting data into training and testing sets.
 
     Parameters
     ----------
@@ -107,7 +107,7 @@ def splitting_data(X, y):
     y_test : DataFrame
         Testing target.
 
-    '''
+    """
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.3, shuffle=True, stratify=y, random_state=42
     )
@@ -115,8 +115,8 @@ def splitting_data(X, y):
 
 
 def oversampling_data(X_train, y_train):
-    '''
-    
+    """
+    Function to perform over-sampling using SMOTE.
 
     Parameters
     ----------
@@ -132,15 +132,15 @@ def oversampling_data(X_train, y_train):
     y_sm : DataFrame
         Training target after oversampling.
 
-    '''
+    """
     sm = SMOTE()
     X_sm, y_sm = sm.fit_resample(X_train, y_train)
     return X_sm, y_sm
 
 
 def machine_learning_model_KNN(n_neighbors=9, weights="uniform", algorithm="auto"):
-    '''
-    
+    """
+    Function used to definie KNN algorithm.
 
     Parameters
     ----------
@@ -156,7 +156,7 @@ def machine_learning_model_KNN(n_neighbors=9, weights="uniform", algorithm="auto
     clf : classifier
         Model used for classification.
 
-    '''
+    """
     clf = KNeighborsClassifier(
         n_neighbors=n_neighbors, weights=weights, algorithm=algorithm
     )
@@ -164,8 +164,8 @@ def machine_learning_model_KNN(n_neighbors=9, weights="uniform", algorithm="auto
 
 
 def learning_model(X_sm, y_sm, clf):
-    '''
-    
+    """
+    Function used to train model.
 
     Parameters
     ----------
@@ -181,14 +181,14 @@ def learning_model(X_sm, y_sm, clf):
     clf : classifier
         Trained classifier.
 
-    '''
-    clf = clf.fit(X_sm, y_sm.values.flatten()) 
+    """
+    clf = clf.fit(X_sm, y_sm.values.flatten())
     return clf
 
 
 def using_model(clf, X_test):
-    '''
-    
+    """
+    Function used to predict targets for training set to measure its scores.
 
     Parameters
     ----------
@@ -202,14 +202,14 @@ def using_model(clf, X_test):
     y_pred : array of int64
         Predicted target for X_test labels.
 
-    '''
+    """
     y_pred = clf.predict(X_test)
     return y_pred
 
 
 def metric(y_test, y_pred):
-    '''
-    
+    """
+    Function used to measure score. Here we have balanced accuracy score.
 
     Parameters
     ----------
@@ -224,14 +224,14 @@ def metric(y_test, y_pred):
         Here is used balanced accuracy score.
         On a slale 0-1 it describes how good our predictions are.
 
-    '''
+    """
     score = balanced_accuracy_score(y_test, y_pred)
     return score
 
 
 def conf_matrix(y_test, y_pred):
-    '''
-    
+    """
+    Function used to generate confusion matrix.
 
     Parameters
     ----------
@@ -243,30 +243,30 @@ def conf_matrix(y_test, y_pred):
     Returns
     -------
     conf_matrix : array of int64
-        Specific table layout that allows visualization of the performance 
+        Specific table layout that allows visualization of the performance
         of an algorithm .
 
-    '''
+    """
     conf_matrix = confusion_matrix(y_test, y_pred)
     return conf_matrix
 
 
 def load_test_data():
-    '''
+    """
     Loading test data
 
     Returns
     -------
     test_data : DataFrame
 
-    '''
+    """
     test_data = pd.read_csv("test_data.csv", header=None)
     return test_data
 
 
 def predict_target_for_test_data(test_data, clf, kpca):
-    '''
-    
+    """
+    Function used to predict target for test data set.
 
     Parameters
     ----------
@@ -282,24 +282,42 @@ def predict_target_for_test_data(test_data, clf, kpca):
     y_test_pred : DataFrame
         Predicted target for input data.
 
-    '''
+    """
     y_test_pred = clf.predict(kpca.transform(test_data))
     y_test_pred = pd.DataFrame(y_test_pred)
     return y_test_pred
 
 
+def y_test_pred_to_csv(y_test_pred, name):
+    """
+    Function used to save predicted target as csv.
+
+    Parameters
+    ----------
+    y_test_pred : DataFrame
+        Predicted target for test data set..
+
+    Returns
+    -------
+    None.
+
+    """
+    y_test_pred.to_csv(f"{name}.csv")
+
+
 if __name__ == "__main__":
     X, y = loading_files(path)
     y_binar = binarization(y)
-    X_pca = principal_component_analysis(X, n_components=1012)
+    X_pca = kernel_principal_component_analysis(X, n_components=1012)
     X_train, X_test, y_train, y_test = splitting_data(X_pca, y_binar)
     X_sm, y_sm = oversampling_data(X_train, y_train)
     clf = machine_learning_model_KNN(n_neighbors=9, weights="uniform", algorithm="auto")
     clf_learned = learning_model(X_sm, y_sm, clf)
-    y_pred = using_model(clf = clf_learned, X_test = X_test)
+    y_pred = using_model(clf=clf_learned, X_test=X_test)
     score = metric(y_test, y_pred)
     conf_matrix = conf_matrix(y_test, y_pred)
-    file = 'KNN.sav'
-    joblib.dump(clf_learned, filename = file)
+    file = "KNN.sav"
+    joblib.dump(clf_learned, filename=file)
     test_data = load_test_data()
-    y_test_pred = predict_target_for_test_data(test_data, clf = clf_learned, kpca = kpca)
+    y_test_pred = predict_target_for_test_data(test_data, clf=clf_learned, kpca=kpca)
+    y_test_pred_to_csv(y_test_pred, name="predicted_test_target")
